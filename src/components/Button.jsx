c03c6ffdf2cc9f2e3926cb8257714b89d00121ea
Sebastian Feleńczak <sebastianfelenczak@gmail.com>
2023-04-14 20:55:15 +0200

    react js empty project

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button = props => {
+  return (
+    <div>
+      <button style={{ width: `${props.width}` }}>Coikolwiek</button>
+    </div>
+  );
+};
+
+export default Button;
